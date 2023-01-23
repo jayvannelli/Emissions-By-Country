8b2d58fe7c54d6af04b93ccafa9f58377f95351d
jayvannelli <jaymvannelli@gmail.com>
2023-01-23 05:28:49 -0500

    Add progress.

---
+++ pages/americas.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from src.data import get_data
+from src.settings import CARIBBEAN, CENTRAL_AMERICA, SOUTH_AMERICA
+
+
+def main():
+    st.title("Americas")
+
+    df = get_data()
+
+    caribbean_tab, central_america_tab, south_america_tab, north_america_tab = st.tabs(
+        ["Caribbean", "Central America", "South America", "North America"]
+    )
+
+    with caribbean_tab:
+        st.subheader("Caribbean")
+        caribbean_selection = st.selectbox("Select country", options=CARIBBEAN)
+
+        caribbean_df = df.loc[df['Country'] == caribbean_selection]
+        st.dataframe(caribbean_df)
+
+    with central_america_tab:
+        st.subheader("Central America")
+        central_america_selection = st.selectbox("Select country", options=CENTRAL_AMERICA)
+
+        central_america_df = df.loc[df['Country'] == central_america_selection]
+        st.dataframe(central_america_df)
+
+    with south_america_tab:
+        st.subheader("South America")
+        south_america_selection = st.selectbox("Select country", options=SOUTH_AMERICA)
+
+        south_america_df = df.loc[df['Country'] == south_america_selection]
+        st.dataframe(south_america_df)
+
+    with north_america_tab:
+        st.subheader("North America")
+        north_america_selection = st.selectbox("Select country", options=["Canada", "Mexico", "USA"])
+
+        north_america_df = df.loc[df['Country'] == north_america_selection]
+        st.dataframe(north_america_df)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/settings.py
@@ -0,0 +1,21 @@
+
+CARIBBEAN: list[str] = [
+    "Anguilla", "Antigua and Barbuda", "Aruba", "Bahamas",
+    "Barbados", "Bermuda", "British Virgin Islands",
+    "Cuba", "Curaçao", "Dominica", "Dominican Republic",
+    "Grenada", "Guadeloupe", "Haiti", "Jamaica", "Martinique",
+    "Montserrat", "Puerto Rico", "Saint Kitts and Nevis",
+    "Saint Lucia", "Saint Vincent and the Grenadines",
+    "Trinidad and Tobago"
+]
+
+CENTRAL_AMERICA: list[str] = [
+    "Belize", "Costa Rica", "El Salvador", "Guatemala",
+    "Honduras", "Nicaragua", "Panama"
+]
+
+SOUTH_AMERICA: list[str] = [
+    "Argentina", "Bolivia", "Brazil", "Chile", "Colombia",
+    "Ecuador", "French Guiana", "Guyana", "Paraguay", "Peru",
+    "Suriname", "Uruguay", "Venezuela"
+]
