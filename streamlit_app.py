acc7cb686c2c9d82559ad74ec64c4938fae06a5c
jayvannelli <jaymvannelli@gmail.com>
2023-01-31 14:53:10 -0500

    Clean main page and adjust default start year for double-year slider.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,8 @@
 import streamlit as st
-from src.data import get_data
 
 
 def main():
     st.title("Emissions by Country | 2002-2022")
-
-    df = get_data()
-
-    country = st.selectbox("Select country:", options=df['Country'].unique())
-    country_df = df.loc[df['Country'] == country]
-
-    st.bar_chart(country_df, x="Year", y="Total")
-    st.dataframe(country_df)
 
 
 if __name__ == "__main__":
