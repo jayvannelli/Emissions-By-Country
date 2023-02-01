f070d6242165967161a26a4f5aca5992de86ee92
jayvannelli <jaymvannelli@gmail.com>
2023-02-01 10:59:59 -0500

    Include y-value select-boxes for africa page.

--- pages/africa.py
+++ pages/africa.py
@@ -1,7 +1,9 @@
+import pandas as pd
 import streamlit as st
 
 from src.data import get_data
 from src.settings import (
+    VALID_VALUE_SELECTIONS,
     NORTH_AFRICA,
     WESTERN_AFRICA,
     SOUTHERN_AFRICA,
@@ -22,46 +24,82 @@ def main():
     with north_africa_tab:
         st.subheader("North Africa")
 
-        north_africa_selection = st.selectbox("Select country", options=NORTH_AFRICA)
-        north_africa_df = df.loc[df['Country'] == north_africa_selection]
+        left_column, right_column = st.columns([3, 1])
 
-        st.bar_chart(north_africa_df, x="Year", y="Total")
+        with left_column:
+            north_africa_selection = st.selectbox("Select country", options=NORTH_AFRICA)
+            north_africa_df = df.loc[df['Country'] == north_africa_selection]
+        with right_column:
+            north_africa_value = st.selectbox(label="Select value to display",
+                                              options=VALID_VALUE_SELECTIONS,
+                                              key="north_africa_value_selection")
+
+        st.bar_chart(north_africa_df, x="Year", y=north_africa_value)
+
         st.dataframe(north_africa_df)
 
     with western_africa_tab:
         st.subheader("Western Africa")
 
-        western_africa_selection = st.selectbox("Select country", options=WESTERN_AFRICA)
-        western_africa_df = df.loc[df['Country'] == western_africa_selection]
+        left_column, right_column = st.columns([3, 1])
 
-        st.bar_chart(western_africa_df, x="Year", y="Total")
+        with left_column:
+            western_africa_selection = st.selectbox("Select country", options=WESTERN_AFRICA)
+            western_africa_df = df.loc[df['Country'] == western_africa_selection]
+        with right_column:
+            western_africa_value = st.selectbox(label="Select value to display",
+                                                options=VALID_VALUE_SELECTIONS,
+                                                key="western_africa_value_selection")
+
+        st.bar_chart(western_africa_df, x="Year", y=western_africa_value)
         st.dataframe(western_africa_df)
 
     with central_africa_tab:
         st.subheader("Central Africa")
 
-        central_africa_selection = st.selectbox("Select country", options=CENTRAL_AFRICA)
-        central_africa_df = df.loc[df['Country'] == central_africa_selection]
+        left_column, right_column = st.columns([3, 1])
 
-        st.bar_chart(central_africa_df, x="Year", y="Total")
+        with left_column:
+            central_africa_selection = st.selectbox("Select country", options=CENTRAL_AFRICA)
+            central_africa_df = df.loc[df['Country'] == central_africa_selection]
+        with right_column:
+            central_africa_value = st.selectbox(label="Select value to display",
+                                                options=VALID_VALUE_SELECTIONS,
+                                                key="central_africa_value_selection")
+
+        st.bar_chart(central_africa_df, x="Year", y=central_africa_value)
         st.dataframe(central_africa_df)
 
     with east_africa_tab:
         st.subheader("East Africa")
 
-        east_africa_selection = st.selectbox("Select country", options=EAST_AFRICA)
-        east_africa_df = df.loc[df['Country'] == east_africa_selection]
+        left_column, right_column = st.columns([3, 1])
 
-        st.bar_chart(east_africa_df, x="Year", y="Total")
+        with left_column:
+            east_africa_selection = st.selectbox("Select country", options=EAST_AFRICA)
+            east_africa_df = df.loc[df['Country'] == east_africa_selection]
+        with right_column:
+            east_africa_value = st.selectbox(label="Select value to display",
+                                             options=VALID_VALUE_SELECTIONS,
+                                             key="east_africa_value_selection")
+
+        st.bar_chart(east_africa_df, x="Year", y=east_africa_value)
         st.dataframe(east_africa_df)
 
     with southern_africa_tab:
         st.subheader("Southern Africa")
 
-        southern_africa_selection = st.selectbox("Select country", options=SOUTHERN_AFRICA)
-        southern_africa_df = df.loc[df['Country'] == southern_africa_selection]
+        left_column, right_column = st.columns([3, 1])
 
-        st.bar_chart(southern_africa_df, x="Year", y="Total")
+        with left_column:
+            southern_africa_selection = st.selectbox("Select country", options=SOUTHERN_AFRICA)
+            southern_africa_df = df.loc[df['Country'] == southern_africa_selection]
+        with right_column:
+            southern_africa_value = st.selectbox(label="Select value to display",
+                                                 options=VALID_VALUE_SELECTIONS,
+                                                 key="southern_africa_value_selection")
+
+        st.bar_chart(southern_africa_df, x="Year", y=southern_africa_value)
         st.dataframe(southern_africa_df)
 
 
