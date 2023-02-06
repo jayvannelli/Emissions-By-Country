334240dbb16cf2ba444edce78d7c52530bd9d030
jayvannelli <jaymvannelli@gmail.com>
2023-02-06 11:35:47 -0500

    Remove unused requirement; update year on license; clean up data pull.

--- src/data.py
+++ src/data.py
@@ -4,5 +4,4 @@ import pandas as pd
 
 @st.cache(allow_output_mutation=True)
 def get_data():
-    _df = pd.read_csv("data/emissions_data.csv")
-    return _df
+    return pd.read_csv("data/emissions_data.csv")
